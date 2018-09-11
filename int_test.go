b101ecc231b335f980323785de62bfc533448abb
vvizard75 <vvizard@vvizard.ru>
2018-09-12 00:59:16 +1000

    split tests

---
+++ int_test.go
@@ -0,0 +1,73 @@
+// +build integration
+
+package main
+
+import (
+	"github.com/labstack/echo"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
+	"testing"
+)
+
+func TestGetFileFromPath_goodImg(t *testing.T) {
+	e := echo.New()
+	fv := make(url.Values)
+	fv.Set("path", "https://cdn.pixabay.com/photo/2015/10/06/19/28/landscape-975091_960_720.jpg")
+
+	req := httptest.NewRequest(echo.POST, "/upload", strings.NewReader(fv.Encode()))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationForm)
+	rec := httptest.NewRecorder()
+
+	c := e.NewContext(req, rec)
+	chooseHandler(c)
+	if rec.Code != http.StatusOK {
+		t.Errorf("Error get file from path!\n Response: %d; %s\n", rec.Code, rec.Body)
+		t.Fail()
+	}
+
+	results, err := getResults(rec, t)
+	if err != nil {
+		t.Errorf("Error parse json response %s\n", err)
+		t.Fail()
+	}
+	if len(results) == 1 {
+		if results[0].Error {
+			t.Errorf("Error upload good image!")
+			t.Fail()
+		} else {
+			for _, value := range results {
+				clearTestData(value, t)
+			}
+		}
+	}
+
+}
+
+func TestGetFileFromPath_badImg(t *testing.T) {
+	e := echo.New()
+	fv := make(url.Values)
+	fv.Set("path", "https://github.com/mushinnomushin/ZeuS_2.0.8.9/blob/master/README.txt")
+
+	req := httptest.NewRequest(echo.POST, "/upload", strings.NewReader(fv.Encode()))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationForm)
+	rec := httptest.NewRecorder()
+
+	c := e.NewContext(req, rec)
+	chooseHandler(c)
+	if rec.Code != http.StatusBadRequest {
+		t.Errorf("Error get file from path!\n Response: %d; %s\n", rec.Code, rec.Body)
+		t.Fail()
+	} else {
+		results, err := getResults(rec, t)
+		if err != nil {
+			t.Errorf("Error parse json response %s\n", err)
+			t.Fail()
+		}
+		if len(results) == 1 && !results[0].Error {
+			t.Errorf("Error recognize bad format of image!")
+		}
+	}
+
+}
--- main_test.go
+++ main_test.go
@@ -17,44 +17,10 @@ import (
 	"testing"
 )
 
-func TestGetFileFromPath_goodImg(t *testing.T) {
-	e := echo.New()
-	fv := make(url.Values)
-	fv.Set("path", "https://cdn.pixabay.com/photo/2015/10/06/19/28/landscape-975091_960_720.jpg")
-
-	req := httptest.NewRequest(echo.POST, "/upload", strings.NewReader(fv.Encode()))
-	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationForm)
-	rec := httptest.NewRecorder()
-
-	c := e.NewContext(req, rec)
-	chooseHandler(c)
-	if rec.Code != http.StatusOK {
-		t.Errorf("Error get file from path!\n Response: %d; %s\n", rec.Code, rec.Body)
-		t.Fail()
-	}
-
-	results, err := getResults(rec, t)
-	if err != nil {
-		t.Errorf("Error parse json response %s\n", err)
-		t.Fail()
-	}
-	if len(results) == 1 {
-		if results[0].Error {
-			t.Errorf("Error upload good image!")
-			t.Fail()
-		} else {
-			for _, value := range results {
-				clearTestData(value, t)
-			}
-		}
-	}
-
-}
-
 func TestGetFileFromPath_badPath(t *testing.T) {
 	e := echo.New()
 	fv := make(url.Values)
-	fv.Set("path", "https://cdn.pixabay.com/photo/2015/10/06/19/28/landscape-975091_960_7201.jpg")
+	fv.Set("path", "./test/testdata/test_pic3.jpg")
 
 	req := httptest.NewRequest(echo.POST, "/upload", strings.NewReader(fv.Encode()))
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationForm)
@@ -72,33 +38,6 @@ func TestGetFileFromPath_badPath(t *testing.T) {
 		}
 		if len(results) == 1 && !results[0].Error {
 			t.Errorf("Found downloaded image by bad path!")
-		}
-	}
-
-}
-
-func TestGetFileFromPath_badImg(t *testing.T) {
-	e := echo.New()
-	fv := make(url.Values)
-	fv.Set("path", "https://github.com/mushinnomushin/ZeuS_2.0.8.9/blob/master/README.txt")
-
-	req := httptest.NewRequest(echo.POST, "/upload", strings.NewReader(fv.Encode()))
-	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationForm)
-	rec := httptest.NewRecorder()
-
-	c := e.NewContext(req, rec)
-	chooseHandler(c)
-	if rec.Code != http.StatusBadRequest {
-		t.Errorf("Error get file from path!\n Response: %d; %s\n", rec.Code, rec.Body)
-		t.Fail()
-	} else {
-		results, err := getResults(rec, t)
-		if err != nil {
-			t.Errorf("Error parse json response %s\n", err)
-			t.Fail()
-		}
-		if len(results) == 1 && !results[0].Error {
-			t.Errorf("Error recognize bad format of image!")
 		}
 	}
 
